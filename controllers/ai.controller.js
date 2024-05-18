22ebde4b35fc65443abbed692ae2e9c608d8e659
0xjesus <pdlgmcn@protonmail.com>
2024-05-18 14:19:58 -0600

    Burrito LLM AI Uncensored Infrastructure

--- controllers/ai.controller.js
+++ controllers/ai.controller.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import axios from 'axios';
 import { promptTokensEstimate } from 'openai-chat-tokens';
 import { getRouter } from '@thewebchimp/primate';
-
+// creation
 const openaiAPIKey = process.env.OPENAI_API_KEY;
 const perplexityAPIKey = process.env.PERPLEXITY_API_KEY;
 const groqAPIKey = process.env.GROQ_API_KEY;
