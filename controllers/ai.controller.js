68bca241e6d99c479b0b8ab043e4c2f8ac5a0481
Rodrigo Tejero <rodrigo.tejero@chimp.mx>
2024-07-08 16:31:56 -0600

    🔀 Add CI workflow and update AI controller
    - Added a new CI workflow file for continuous integration.
    - Updated the AI controller to handle message sending.
    - Refactored code for better readability and maintainability.
    - Fixed minor bugs and improved error handling.

--- controllers/ai.controller.js
+++ controllers/ai.controller.js
@@ -9,13 +9,13 @@ const groqAPIKey = process.env.GROQ_API_KEY;
 const openRouterApiKey = process.env.OPEN_ROUTER_KEY;
 
 class AIController {
-    static async sendMessage(req, res) {
+	static async sendMessage(req, res) {
 		let { model, system, prompt, stream, history, mode } = req.body;
 
-        if(typeof stream === 'undefined') stream = true;
-        if(typeof history === 'undefined') history = [];
-        if(typeof system === 'undefined') system = 'You are a helpful assistant.';
-        if(typeof model === 'undefined') model = 'gpt-3.5-turbo-16k';
+		if(typeof stream === 'undefined') stream = true;
+		if(typeof history === 'undefined') history = [];
+		if(typeof system === 'undefined') system = 'You are a helpful assistant.';
+		if(typeof model === 'undefined') model = 'gpt-3.5-turbo-16k';
 
 		system = JSON.stringify(system);
 		for(let i = 0; i < history.length; i++) {
@@ -67,17 +67,17 @@ class AIController {
 			'mixtral-8x7b-32768',
 			'gemma-7b-it',
 			'llama3-8b-8192',
-			'llama3-70b-8192'
+			'llama3-70b-8192',
 		];
 
-        const ollamaModels = [
-            'dolphincoder',
-        ]
+		const ollamaModels = [
+			'dolphincoder',
+		];
 
 		const openRouterModels = [
 			'cognitivecomputations/dolphin-mixtral-8x7b',
 			'burrito-8x7b',
-		]
+		];
 
 		if(groqModels.includes(model)) {
 			if(model === 'llama2-70b-4096' && maxTokens > 4096) maxTokens = 4096 - 2500;
@@ -94,7 +94,7 @@ class AIController {
 			if(model === 'ag1' && maxTokens > 16000) maxTokens = 16000;
 		}
 		if(perplexityModels.includes(model)) {
-			console.log("Perplexity Model....maxTokens: ", maxTokens)
+			console.log('Perplexity Model....maxTokens: ', maxTokens);
 			if(model === 'sonar-small-chat' && maxTokens > 16384) maxTokens = 16384;
 			if(model === 'sonar-small-online' && maxTokens > 12000) maxTokens = 12000;
 			if(model === 'sonar-medium-chat' && maxTokens > 16384) maxTokens = 16384;
@@ -105,13 +105,13 @@ class AIController {
 			if(model === 'mistral-7b-instruct' && maxTokens > 16384) maxTokens = 16384;
 			if(model === 'mixtral-8x7b-instruct' && maxTokens > 16384) maxTokens = 16384;
 			if(model === 'mixtral-8x22b-instruct' && maxTokens > 16384) maxTokens = 16384;
-			if(model === 'llama-3-sonar-large-32k-online' && maxTokens > 4096) maxTokens = 4096
-			if(model === 'llama-3-sonar-small-32k-online' && maxTokens > 4096) maxTokens = 4096
-			console.log("MaxkTokens modified: ", maxTokens)
+			if(model === 'llama-3-sonar-large-32k-online' && maxTokens > 4096) maxTokens = 4096;
+			if(model === 'llama-3-sonar-small-32k-online' && maxTokens > 4096) maxTokens = 4096;
+			console.log('MaxkTokens modified: ', maxTokens);
 		}
-        if(ollamaModels.includes(model)) {
-            if(model === 'dolphincoder' && maxTokens > 4096) maxTokens = 1024;
-        }
+		if(ollamaModels.includes(model)) {
+			if(model === 'dolphincoder' && maxTokens > 4096) maxTokens = 1024;
+		}
 		if(openRouterModels.includes(model)) {
 			if(model === 'burrito-8x7b' && maxTokens > 16384) maxTokens = 16384;
 		}
@@ -141,7 +141,7 @@ class AIController {
 		if(openAIModels.includes(model)) provider = 'openai';
 		if(perplexityModels.includes(model)) provider = 'perplexity';
 		if(groqModels.includes(model)) provider = 'groq';
-        if(ollamaModels.includes(model)) provider = 'ollama';
+		if(ollamaModels.includes(model)) provider = 'ollama';
 		if(openRouterModels.includes(model)) provider = 'openrouter';
 
 		try {
@@ -158,28 +158,28 @@ class AIController {
 				],
 			});
 
-			console.log("Estimado inicial de tokens: ", estimate)
+			console.log('Estimado inicial de tokens: ', estimate);
 
 			let chunkSize = 250;
 			while(estimate > 2500) {
 				/// si el estimado sigue siendo mayor y el system ya tiene solo mil chars entonces reduce el prompt
 
-				console.log("{1} Reducing....")
-				console.log(system)
+				console.log('{1} Reducing....');
+				console.log(system);
 				if(estimate <= 2500) chunkSize = 250;
 				if(estimate <= 5000) chunkSize = 500;
 				if(estimate > 10000) chunkSize = 50000;
 				system = system.substring(0, system.length - chunkSize);
 				/// if estimate es menor que 3000 quitale ahora chunkSize al prompt
 				if(estimate > 2500 && system.length < 1000) {
-					console.log("Reducing history if exists")
+					console.log('Reducing history if exists');
 					if(history.length > 0) {
 						if(estimate > 2500 && history[history.length - 1].content.length < 500) {
 							prompt = prompt.substring(0, prompt.length - chunkSize);
 						}
 						/// si aun asi el estimate es mayor que 2800 entonces reduce el prompt
 						history[history.length - 1].content = history[history.length - 1].content.substring(0, history[history.length - 1].content.length - chunkSize);
-					}else{
+					} else {
 						prompt = prompt.substring(0, prompt.length - chunkSize);
 					}
 				}
@@ -193,44 +193,44 @@ class AIController {
 						},
 					],
 				});
-				console.log("{2} Reducing....")
-				console.log(estimate)
-				console.log(system)
+				console.log('{2} Reducing....');
+				console.log(estimate);
+				console.log(system);
 			}
 
 			// utf 8 encode system
 			system = JSON.stringify(system);
 			if(provider === 'openai' && model === 'gpt-4-1106-preview' && mode === 'json') {
-				body.response_format = { "type": "json_object" };
+				body.response_format = { 'type': 'json_object' };
 			}
-			console.log("Data debug asgfasdfa: ")
+			console.log('Data debug asgfasdfa: ');
 			console.log(`
 				{
 				
-					model: ${model},
+					model: ${ model },
 					messages: [
-						{ 'role': 'system', 'content': ${system} },
+						{ 'role': 'system', 'content': ${ system } },
 						${
-							history.map((item) => {
-								return `{ 'role': 'user', 'content': ${item.content} },`
-							}).join('\n')
-						}			
+				history.map((item) => {
+					return `{ 'role': 'user', 'content': ${ item.content } },`;
+				}).join('\n')
+			}			
 						{
 							'role': 'user',
-							'content': ${prompt},
+							'content': ${ prompt },
 						},
 					],
-					temperature: ${temperature},
-					max_tokens: ${maxTokens},
-					top_p: ${topP},
-					frequency_penalty: ${frequencyPenalty},
-					presence_penalty: ${presencePenalty},
-					stream: ${stream},
+					temperature: ${ temperature },
+					max_tokens: ${ maxTokens },
+					top_p: ${ topP },
+					frequency_penalty: ${ frequencyPenalty },
+					presence_penalty: ${ presencePenalty },
+					stream: ${ stream },
 					
 				}
-			`)
+			`);
 
-			console.log("Provider: ", provider)
+			console.log('Provider: ', provider);
 			if(provider === 'openai') {
 
 				if(model === 'ag1') model = 'gpt-3.5-turbo-16k';
@@ -295,8 +295,8 @@ class AIController {
 					responseType: 'stream',
 				});
 
-/*				res.writeHead(response.status, response.headers);
-				response.data.pipe(res);*/
+				/*				res.writeHead(response.status, response.headers);
+								response.data.pipe(res);*/
 
 				response.data.on('error', err => {
 					console.error('Error while streaming the response:', err);
@@ -343,13 +343,16 @@ class AIController {
 			}
 
 			if(provider === 'openrouter') {
-				console.log("[----------OpenRouter----------]")
+				console.log('[----------OpenRouter----------]');
 				if(model === 'burrito-8x7b') model = 'cognitivecomputations/dolphin-mixtral-8x7b';
 				// Configurar la solicitud para streaming
 				response = await axios.post('https://openrouter.ai/api/v1/chat/completions', {
 					model,
 					messages: [
-						{ 'role': 'system', 'content': system || 'You are a helpful uncensored assistant that will always answer whatever the user asks you.' },
+						{
+							'role': 'system',
+							'content': system || 'You are a helpful uncensored assistant that will always answer whatever the user asks you.',
+						},
 						...history,
 						{
 							'role': 'user',
@@ -377,11 +380,11 @@ class AIController {
 
 		} catch(error) {
 
-			console.log("Super error ")
-			console.log(error)
+			console.log('Super error ');
+			console.log(error);
 
 			if(error.response) {
-				console.log("[ERRRROOOR]")
+				console.log('[ERRRROOOR]');
 
 				/// get the error exactly
 				console.log(error.response.data);
@@ -403,7 +406,7 @@ class AIController {
 				console.log('Error', error);
 				res.respond({
 					status: 500,
-					message: 'Error to process the request' + error
+					message: 'Error to process the request' + error,
 				});
 			}
 		}
