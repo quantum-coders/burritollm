87df050980fa0c43d3c40aeb7f31fe00720a98ec
Rodrigo Tejero <rodrigo.tejero@chimp.mx>
2024-08-09 08:16:54 -0600

    🔧 Add wait list functionality
    - Added a new model `WaitList` to the Prisma schema
    - Created a new route `/wait-list` in `default.js` to handle adding email addresses to the wait list
    - Implemented logic to create a new entry in the `WaitList` table using Prisma's `create` method
    - Handled success and error responses when adding an email address to the wait list

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 import primate from '@thewebchimp/primate';
 import { router as ai } from './routes/ai.js';
 import { router as base } from './routes/default.js';
-import {router as web3 } from './routes/web3.js';
-import * as bodyParser from "express";
+import { router as web3 } from './routes/web3.js';
+
 await primate.setup();
+
 primate.app.use('/', base);
 primate.app.use('/ai', ai);
 primate.app.use('/web3', web3);
